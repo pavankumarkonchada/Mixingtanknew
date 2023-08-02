db3b2885e8009ad0dde2bb26065b022a20a50d88
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-02 14:10:38 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 # Add the 'lib' folder to the Python path
 lib_path = os.path.join(os.path.dirname(__file__), 'lib')
 sys.path.append(lib_path)
-import lib.pymapdl.remote_bimetallic
+#import lib.pymapdl.remote_bimetallic
 from lib.constants import *
 
 
