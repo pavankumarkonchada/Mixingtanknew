dc385b8c1bc90a2ab84ce409a13b0060911a73f8
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-02 13:24:39 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 #importing web application related modules
 #import flask
 from flask import Flask, render_template, request
+import sys
+import os
+
+# Add the 'lib' folder to the Python path
+lib_path = os.path.join(os.path.dirname(__file__), 'lib')
+sys.path.append(lib_path)
 
 
 app = Flask(__name__)
