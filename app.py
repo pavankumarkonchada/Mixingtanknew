c129de64b67965da9665d25fa13166147890f6b0
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-02 17:10:07 +0530

    app.py

--- app.py
+++ app.py
@@ -37,19 +37,4 @@ def calculator():
     #these flag variables are used to identify if the solution has run or not
     flag = ''
     flag2=''
-        return render_template('inputpage.html',
-                           Flag = 0,
-                           SolveStatus='Solved',
-                           output_image_url=image,
-                           L2=my_boundary,
-                           t2=my_growth,
-                           E21=my_cores,
-                           E22=my_flow,
-                           c21=my_meshsize,
-                           Flag2=flag2,
-                           ip=inlet_press,
-                           ws=shear_int,
-                           C1=out_len,
-                           C11=in1_len,
-                           C111=in2_len,
-                           C1111=imp_rad)
+        return render_template('inputpage.html')
