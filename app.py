c736d59f9e98bfff167c0c935b47c2ec27989477
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-02 13:20:18 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 #importing web application related modules
 #import flask
 from flask import Flask, render_template, request
-import lib.pymapdl.remote_bimetallic
-from lib.constants import *
+
 
 app = Flask(__name__)
 
