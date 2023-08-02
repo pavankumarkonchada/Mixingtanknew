fe6be0ca3e0e6b41d38b9555232404c18063f488
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-02 12:40:00 +0530

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-#importing web application related modules
-#import flask
 from flask import Flask, render_template
 
-
-#an app instance is opened using variable app
 app = Flask(__name__)
 
-
-
-
-#defining the app extension (This case has just '/', meaning this is the main domain)
-@app.route("/")
-def calculator():  
-    return render_template('inputpage.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',passthrough_errors=True,use_reloader=False,port=5000)
+@app.route('/')
+def index():
+    return render_template('index.html', title='My Flask App')
