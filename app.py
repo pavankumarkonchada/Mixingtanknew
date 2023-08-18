7268190a60663b9fb78acdf3a0ff75463cbbbb39
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-18 18:37:01 +0530

    app.py

--- app.py
+++ app.py
@@ -31,7 +31,10 @@ connect_str = f"DefaultEndpointsProtocol=https;AccountName=trailmixin;AccountKey
 container_name = "mixstore" # container name in which images will be store in the storage account
 print("Connection String:", connect_str)
 
-
+account_name = 'cadfemstorage'
+account_key = '2Q+8yku1CsKNxavljdSnybnyviX1scDZrLgggdnk54R3i7V7KVxNv2YVDXvuSLZy9TeC03KmeIQb+AStelJlnA=='
+container_name = 'new-container'
+blob_name = 'mixing_tank_pyfluent.py'
 
 def geomext(filename):
     if not "." in filename:
@@ -46,6 +49,20 @@ def pyFluent(boundary,growth,cores,flow,mesh,files,wd,out,in1,in2,imp):
     print("entering the function")
     lib.pymapdl.remote_bimetallic.solve_mix(boundary,growth,cores,flow,mesh,files,wd,out,in1,in2,imp)
 
+def index():
+    input_str1 = r"C:\check\geom1.scdoc"
+    input_str2 = "8"
+    input_str3 = "1.2"
+    blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    current_content = blob_client.download_blob().readall().decode('utf-8')
+
+    # Replace the old input strings with the new ones
+    updated_content = current_content.replace('import_filename', input_str1).replace('process_count', input_str2).replace('max_size', input_str3)
+    blob_client.upload_blob(updated_content, overwrite=True)
+
+    return render_template('index.html')
 
 
 @app.route("/", methods=['POST', 'GET'])
