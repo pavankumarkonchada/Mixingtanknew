dfc1638b67818efb279d97b174ba6322ea133339
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-23 19:08:28 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+import paramiko
 import requests
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
-@app.route("/", methods=['POST'])
+
+
+# Azure VM details
+azure_vm_ip = "your_vm_ip_address"
+azure_vm_username = "your_vm_username"
+azure_vm_password = "your_vm_password"
+
+@app.route("/transfer", methods=["GET"])
 def process_file():
     try:
         # Read user input from the HTML form
@@ -16,10 +23,10 @@ def process_file():
         github_file_content = response.text
         
         # Modify the content based on user input
-        modified_content = github_file_content.replace('max_size', str(input_value))
-        
+        modified_content = github_file_content.replace('max_size',str(input_value))
+        print("Running adfajspdfj kajsfpaksdjf")
         # Send the modified content to the VM through API
-        vm_api_url = 'http://20.163.248.81/api/endpoint'
+        vm_api_url = 'http://20.163.248.81:80/api/endpoint'
         vm_api_data = {'content': modified_content}
         response = requests.post(vm_api_url, json=vm_api_data)
         
@@ -27,6 +34,5 @@ def process_file():
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
-
-if __name__ == '__main__':   
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
