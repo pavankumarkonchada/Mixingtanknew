14b8d4390585e923546765d59748f63035bbf012
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-23 11:35:58 +0530

    app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def index():
     updated_content = current_content.replace('import_filename', input_str1).replace('process_count', input_str2).replace('max_size', input_str3)
     blob_client2.upload_blob(updated_content, overwrite=True)
     file_name = "remote_bimetallic.py"
-    vm_location = r"C\check"  # Location on the VM to copy the file to
+    vm_location = r"C:\check"  # Location on the VM to copy the file to
 
     blob_content = blob_client.download_blob().readall()
 
@@ -104,15 +104,15 @@ def index():
         f.write(blob_content)
 #code to transfer files to VM is working till this point
     # Use paramiko to run PowerShell commands on the Windows VM
-    #ssh_client = paramiko.SSHClient()
-    #ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #ssh_client.connect(vm_ip, username=vm_username, password=vm_password)
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(vm_ip, username=vm_username, password=vm_password)
         
     # Run PowerShell command to copy the file to the VM
-    #powershell_command = f'Copy-Item "{local_file_path}" "{vm_location}"'
-    #ssh_stdin, ssh_stdout, ssh_stderr = ssh_client.exec_command(f'powershell -command "{powershell_command}"')
+    powershell_command = f'Copy-Item "{local_file_path}" "{vm_location}"'
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh_client.exec_command(f'powershell -command "{powershell_command}"')
 
-    #ssh_client.close()
+    ssh_client.close()
 
     return render_template('index.html')
 
