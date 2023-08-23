73b2d5610c5533f9fd774bb4cabedcf5551fb9a1
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-23 16:11:19 +0530

    app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def pyFluent(boundary,growth,cores,flow,mesh,files,wd,out,in1,in2,imp):
 def process_file():
     try:
         # Read user input from the HTML form
-        input_value = r"C:\check"
+        input_value = 1.2
         
         # Read the file from the GitHub repository
         github_file_url = 'https://github.com/pavankumarkonchada/Mixingtanknew/blob/main/lib/pymapdl/mixing_tank_pyfluent.py'
@@ -74,7 +74,7 @@ def process_file():
         github_file_content = response.text
         
         # Modify the content based on user input
-        modified_content = github_file_content.replace('import_filename', input_value)
+        modified_content = github_file_content.replace('max_size',str(input_value))
         
         # Send the modified content to the VM through API
         vm_api_url = 'http://20.163.248.81:3389/api/endpoint'
