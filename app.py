a22e425200a2ce096b3cc23053f0d373920b6acf
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-24 17:40:12 +0530

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
-from flask import Flask, jsonify
-import paramiko
-import requests
-import io  # Import the io module
+from flask import Flask
 import socket
+import paramiko
+
 app = Flask(__name__)
 
 # Azure VM details
-
+windows_vm_ip = "20.163.248.81"
+ssh_port = 22  # SSH port
 
 def test_connection(host, port):
     try:
-        socket.create_connection((host, port))
+        socket.create_connection((host, port), timeout=10)  # Added timeout for better handling
         return True
     except (ConnectionRefusedError, TimeoutError):
         return False
 
-windows_vm_ip = "20.163.248.81"
-ssh_port = 22  # SSH port
-azure_vm_ip = '20.163.248.81'
-azure_vm_username = 'pavan'
-azure_vm_password = 'Cadfemindia@2023'
-
 @app.route("/")
 def open_notepad():
-if test_connection(windows_vm_ip, ssh_port):
-    print("Connection successful")
-else:
-    print("Connection failed")
+    if test_connection(windows_vm_ip, ssh_port):
+        return "Connection successful"
+    else:
+        return "Connection failed"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
