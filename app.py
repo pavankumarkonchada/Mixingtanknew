8d739d40d8ec76a2bb2a956148aa62e3093d4437
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-24 16:51:56 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 import paramiko
-import requests  # Import the requests module
-import io
+import requests
 
 app = Flask(__name__)
 
@@ -17,9 +16,11 @@ def open_notepad():
         username = 'pavan'
         host = '20.163.248.81'
 
-        # Establish SSH connection
+        # Fetch the private key content
         private_key_content = requests.get(private_key_url).text
-        private_key = paramiko.RSAKey(file_obj=io.StringIO(private_key_content))
+
+        # Establish SSH connection
+        private_key = paramiko.RSAKey.from_private_key_string(private_key_content)
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(hostname=host, username=username, pkey=private_key)
