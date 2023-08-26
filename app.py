629ac230b037a5d7b47d0ff970f6d6af13326027
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-26 12:01:18 +0530

    app.py

--- app.py
+++ app.py
@@ -1,39 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, request, render_template
 import paramiko
-import os
-import base64
 
 app = Flask(__name__)
 
-# Azure VM details
-azure_vm_ip = '20.163.248.81'
-azure_vm_username = 'pavan'
-azure_vm_password = 'Cadfemindia@2023'
+@app.route('/')
 
-@app.route("/")
-def open_notepad():
+def copy_file():
+    source_file_path = 'id_rsa'  # Adjust this path
+    destination_path = '/mnt/mydisk'    # Adjust this path
+    vm_ip_address = '52.249.184.159'
+    username = 'pavan'
+    password = 'Cadfemindia@2023'
+
     try:
-        # Retrieve Private Key from Environment Variable
-        private_key_base64 = os.environ.get('PrivateKey')
-        private_key_bytes = base64.b64decode(private_key_base64)
-
-        # SSH Connection to Azure VM
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        private_key_obj = paramiko.RSAKey.from_private_key(private_key_bytes)
-        ssh.connect(azure_vm_ip, username=azure_vm_username, pkey=private_key_obj)
-
-        # Execute the command remotely (open Notepad)
-        stdin, stdout, stderr = ssh.exec_command('notepad.exe')
-        output = stdout.read().decode()
-
-        ssh.close()
-
-        return jsonify({"result": output})
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(vm_ip_address, username=username, password=password)
+        
+        # SCP the file
+        with ssh_client.open_sftp() as sftp:
+            sftp.put(source_file_path, destination_path)
+        
+        ssh_client.close()
+        return 'File copied successfully!'
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return f'Error: {e}'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+if name == '__main__':
+    app.run(debug=True)
