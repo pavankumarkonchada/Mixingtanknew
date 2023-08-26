6cb2e4be511e831bb6f692370c8f73f2a85bfe7c
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-26 14:56:23 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+#from flask import Flask, request, render_template
 import paramiko
 
 app = Flask(__name__)
@@ -18,13 +18,13 @@ def copy_file():
         ssh_client.connect(vm_ip_address, username=username, password=password)
         print("reaching past SSH connection")
         # SCP the file
-        ftp_client = paramiko.FTPClient(ssh_client)
-        response = ftp_client.sendcmd('PWD')
+        #ftp_client = paramiko.FTPClient(ssh_client)
+        #response = ftp_client.sendcmd('PWD')
         #sftp = ssh_client.open_sftp()
         #sftp.put(source_file_path, destination_path)
         #with ssh_client.open_sftp() as sftp:
         #    sftp.put(source_file_path, destination_path)
-        
+        ssh_client.exec_command("hostname")
         ssh_client.close()
         return 'File copied successfully!'
     except Exception as e:
