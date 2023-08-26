d5c37d14615a9a2bbf938549190a0cd5d5642d83
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-26 14:49:42 +0530

    app.py

--- app.py
+++ app.py
@@ -12,23 +12,23 @@ def copy_file():
     username = 'pavan'
     password = 'Cadfemindia@2023'
     print("entering function")
-    #try:
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(vm_ip_address, username=username, password=password)
-    print("reaching past SSH connection")
+    try:
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(vm_ip_address, username=username, password=password)
+        print("reaching past SSH connection")
         # SCP the file
-    ftp_client = paramiko.FTPClient(ssh_client)
-    response = ftp_client.sendcmd('PWD')
-    #sftp = ssh_client.open_sftp()
-    #sftp.put(source_file_path, destination_path)
+        ftp_client = paramiko.FTPClient(ssh_client)
+        response = ftp_client.sendcmd('PWD')
+        #sftp = ssh_client.open_sftp()
+        #sftp.put(source_file_path, destination_path)
         #with ssh_client.open_sftp() as sftp:
         #    sftp.put(source_file_path, destination_path)
         
-    ssh_client.close()
-    return 'File copied successfully!'
-    #except Exception as e:
-    #    return f'Error: {e}'
+        ssh_client.close()
+        return 'File copied successfully!'
+    except Exception as e:
+        return f'Error: {e}'
 
 if __name__ == '__main__':
     app.run(debug=True)
