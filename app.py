843ed35b5a288b892f41c1c506a3b95978124dab
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-26 13:20:35 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def copy_file():
     source_file_path = 'id_rsa'  # Adjust this path
     destination_path = '/mnt/mydisk'    # Adjust this path
-    vm_ip_address = '52.249.184.159:3389'
+    vm_ip_address = '52.249.184.159'
     username = 'pavan'
     password = 'Cadfemindia@2023'
     print("entering function")
