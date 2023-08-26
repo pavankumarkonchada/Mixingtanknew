1eb0397af0d542f2b174ff2022c1779fe7320961
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-26 15:13:19 +0530

    app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def copy_file():
         # SCP the file
         #ftp_client = paramiko.FTPClient(ssh_client)
         #response = ftp_client.sendcmd('PWD')
-        sftp = ssh_client.open_sftp()
-        sftp.put(source_file_path, destination_path)
+        #sftp = ssh_client.open_sftp()
+        #sftp.put(source_file_path, destination_path)
         #with ssh_client.open_sftp() as sftp:
         #    sftp.put(source_file_path, destination_path)
         #ssh_client.exec_command("hostname")
