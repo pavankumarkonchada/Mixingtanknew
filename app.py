3009cbd52b7aa2b4d479b3b11519a64b6858b489
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-26 13:32:15 +0530

    app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def copy_file():
         ssh_client.connect(vm_ip_address, username=username, password=password)
         print("reaching past SSH connection")
         # SCP the file
-        with ssh_client.open_sftp() as sftp:
-            sftp.put(source_file_path, destination_path)
+        #with ssh_client.open_sftp() as sftp:
+        #    sftp.put(source_file_path, destination_path)
         
         ssh_client.close()
         return 'File copied successfully!'
