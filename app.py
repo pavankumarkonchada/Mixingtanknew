b63b62786236555562daf0473d3bead82a9c2f92
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-26 14:34:30 +0530

    app.py

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ def copy_file():
     ssh_client.connect(vm_ip_address, username=username, password=password)
     print("reaching past SSH connection")
         # SCP the file
-    sftp = ssh_client.open_sftp()
-    sftp.put(source_file_path, destination_path)
+    ftp_client = paramiko.FTPClient(ssh_client)
+    response = ftp_client.sendcmd('PWD')
+    #sftp = ssh_client.open_sftp()
+    #sftp.put(source_file_path, destination_path)
         #with ssh_client.open_sftp() as sftp:
         #    sftp.put(source_file_path, destination_path)
         
