da90e3d328261da801b4e1bd0438d7b6e0a8d7ac
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-28 20:39:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask
 import paramiko
-import os
 
 app = Flask(__name__)
 
@@ -21,12 +20,13 @@ def copy_and_open_file():
         with ssh_client.open_sftp() as sftp:
             sftp.put(source_file_path, destination_path)
 
-        # Open the file using Notepad
-       
+        # Open the file using Notepad++
+        command = f'notepad++.exe {destination_path.replace("\\", "\\\\")}'
+        stdin, stdout, stderr = ssh_client.exec_command(command)
 
         ssh_client.close()
         
-        return "<h1 style='color:red'>File copied and opened successfully</h1>"
+        return "<h1 style='color:red'>File copied and opened with Notepad++ successfully</h1>"
     except Exception as e:
         # Log the exception for troubleshooting
         app.logger.error(f'Error occurred: {e}')
