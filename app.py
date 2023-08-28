58ab659d57c4ee736743b0b65042600ba52321a2
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-28 20:20:15 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def copy_file():
+def copy_and_open_file():
     source_file_path = 'lib/pymapdl/mixing_tank_pyfluent.py'  # Adjust this path
     destination_path = 'C:\\mixing_tank_pyfluent1.py'  # Adjust this path
     vm_ip_address = '13.68.168.34'
@@ -21,9 +21,13 @@ def copy_file():
         with ssh_client.open_sftp() as sftp:
             sftp.put(source_file_path, destination_path)
 
+        # Open the file using Notepad
+        command = f'notepad.exe {destination_path.replace("\\", "\\\\")}'
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+
         ssh_client.close()
         
-        return "<h1 style='color:red'>File copied successfully</h1>"
+        return "<h1 style='color:red'>File copied and opened successfully</h1>"
     except Exception as e:
         # Log the exception for troubleshooting
         app.logger.error(f'Error occurred: {e}')
