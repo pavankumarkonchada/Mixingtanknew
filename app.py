a2aa2f46f259f5de58d15f2c86af1f24cd434856
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-29 10:41:57 +0530

    app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def copy_and_open_file():
 
         # Open the file using Notepad++
         #command = r'notepad++.exe {destination_path.replace("\\", "\\\\")}'
-        command=r'powershell.exe -File C:\run.ps1'
+        command=r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File C:\run.ps1'
         
         stdin, stdout, stderr = ssh_client.exec_command(command)
         paramiko.util.log_to_file('sssh.log')
