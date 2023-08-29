4946fcc81242da3aa43a4a35b5748cffe4baa1b4
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-29 17:53:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,40 +4,29 @@ import paramiko
 app = Flask(__name__)
 
 @app.route('/')
-def copy_and_open_file():
-    source_file_path = 'lib/pymapdl/mixing_tank_pyfluent.py'  # Adjust this path
-    destination_path = 'C:\\mixing_tank_pyfluent1.py'  # Adjust this path
+def launch_fluent():
     vm_ip_address = '13.68.168.34'
     username = 'pavan'
     password = 'Cadfemindia@2023'
+    command_to_execute = 'python -c "from ansys.fluent.core import launch_fluent;import ansys.fluent.core as pyfluent;pyfluent.launch_fluent(precision="double", processor_count=4, mode="meshing",show_gui =False)"'
     
     try:
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(vm_ip_address, username=username, password=password)
 
-        # SCP the file
-        with ssh_client.open_sftp() as sftp:
-            sftp.put(source_file_path, destination_path)
+        # Execute the Fluent launch command on remote VM
+        stdin, stdout, stderr = ssh_client.exec_command(command_to_execute)
 
-        # Open the file using PowerShell
-        channel = ssh_client.invoke_shell()
-        command = r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File C:\run.ps1'
-        channel.send(command)
-
-        output = ""
-        while True:
-            if channel.recv_ready():
-                output += channel.recv(1024).decode()
-            if channel.exit_status_ready():
-                break
+        # Capture and process output
+        output = stdout.read().decode()
+        error = stderr.read().decode()
 
         paramiko.util.log_to_file('sssh.log')
         ssh_client.close()
         
-        return f"<pre>{output}</pre>"
+        return f"<pre>Output: {output}\nError: {error}</pre>"
     except Exception as e:
-        # Log the exception for troubleshooting
         app.logger.error(f'Error occurred: {e}')
         return f'Error: {e}'
 
