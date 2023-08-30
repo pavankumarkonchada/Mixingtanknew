c3160ed3c92fbcd492907b437d7074c71879f884
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-30 15:11:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ def launch_fluent():
         username = 'pavan'
         password = 'Cadfemindia@2023'
         
-        ansys_fluent_path = r'C:\\Program Files\\ANSYS Inc\\ANSYS Student\\v231\\fluent\\ntbin\\win64'
-        python_executable = r'C:\\Users\\pavan\\AppData\\Local\\Programs\\Python\\Python311\\python.exe'
-        remote_script_path = r'C:\\mixingtank_pyfluent.py'  # Path to the script on the remote VM
-        env={'AWP_ROOT231': 'C:\Program Files\ANSYS Inc\ANSYS Student\v231'}
+        ansys_install_path = r'C:\Program Files\ANSYS Inc\ANSYS Student\v231'
+        python_executable = r'C:\Users\pavan\AppData\Local\Programs\Python\Python311\python.exe'
+        remote_script_path = r'C:\mixingtank_pyfluent.py'  # Path to the script on the remote VM
+        env={'AWP_ROOT231': r'C:\Program Files\ANSYS Inc\ANSYS Student\v231'}
+        
         # Construct the command with environment variables and the actual command                                              
         command_to_execute = (
-            f'setx PATH "%PATH%;{ansys_fluent_path}" && '
+            f'setx PATH "%PATH%;{ansys_install_path}\\fluent\\ntbin\\win64" && '
+            f'setx AWP_ROOT231 "{ansys_install_path}" && '
+            f'ssh -t {username}@{vm_ip_address} '
             f'"{python_executable}" "{remote_script_path}"'
         )
 
@@ -24,7 +27,7 @@ def launch_fluent():
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(vm_ip_address, username=username, password=password)
 
-        # Execute the Fluent script on remote VM
+        # Execute the SSH command
         stdin, stdout, stderr = ssh_client.exec_command(command_to_execute, environment=env)
 
         # Capture and process output
