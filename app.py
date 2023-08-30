99a1712268afb2d4f11ebfa8903e0ae25c86d824
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-08-30 12:45:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,20 @@ def launch_fluent():
         password = 'Cadfemindia@2023'
         
         ansys_fluent_path = r'C:\\Program Files\\ANSYS Inc\\ANSYS Student\\v231\\fluent\\ntbin\\win64'
+        python_executable = r'C:\\Users\\pavan\\AppData\\Local\\Programs\\Python\\Python311\\python.exe'
+        
+        remote_script_path = r'C:\\mixingtank_pyfluent.py'  # Path to the script on the remote VM
+
         command_to_execute = (
             f'setx PATH "%PATH%;{ansys_fluent_path}" && '
-            r'cmd /C C:\\Users\\pavan\\AppData\\Local\\Programs\\Python\\Python311\\python.exe '
-            '-c "from ansys.fluent.core import launch_fluent;'
-            'import ansys.fluent.core as pyfluent;'
-            'meshing=pyfluent.launch_fluent(precision=\'double\', processor_count=4, mode=\'meshing\', show_gui=False);'
-            'meshing.workflow.InitializeWorkflow(WorkflowType=\'Watertight Geometry\');'
-            'path=r\\"C:\\\\check\\\\geom1.scdoc\\";'
-            'meshing.workflow.TaskObject[\\"Import Geometry\\"].Arguments = {"FileName": path,"LengthUnit": "mm",}"'
+            f'"{python_executable}" "{remote_script_path}"'
         )
 
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(vm_ip_address, username=username, password=password)
 
-        # Execute the Fluent launch command on remote VM
+        # Execute the Fluent script on remote VM
         stdin, stdout, stderr = ssh_client.exec_command(command_to_execute)
 
         # Capture and process output
