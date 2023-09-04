2c8f2a8ab6f5d92538e9a881fd69fc12d595cbce
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-09-04 17:13:32 +0530

    app.py

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask
 import paramiko
 #from pypsexec.client import Client
 import winrm
-import socket
-import Xlib.support.connect as xlib_connect
-import logging
-import select
-import os
+from pypsexec.client import Client
 
-LOGGER = logging.getLogger(__name__)
+
 app = Flask(__name__)
 app.config['REQUEST_TIMEOUT'] = 300  # Set your desired timeout value in seconds
 
@@ -25,12 +21,8 @@ def launch_fluent():
         
         ssh_client = paramiko.SSHClient()
         ssh_config=paramiko.SSHConfig()
-        ssh_config.parse(open(r'C:\programdata\ssh\ssh_config'))
-        #ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(vm_ip_address, username=username, password=password, config=ssh_config)
-
-        transport=ssh_client.get_transport()
-        transport.set_x11_forwarding(True)
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(vm_ip_address, username=username, password=password)
         
         #s = winrm.Session('13.68.168.34', auth=('pavan', 'Cadfemindia@2023'))
         #r = s.run_cmd('notepad.exe')
@@ -40,26 +32,26 @@ def launch_fluent():
         #r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File C:\run.ps1'#(f'"{ansys_fluent_path}\\fluent.exe" 3ddp -meshing -gu -ssh -wait')
 
         # Execute the Fluent launch command on remote VM
-        stdin, stdout, stderr = ssh_client.exec_command(fluent_command)
+        #stdin, stdout, stderr = ssh_client.exec_command(fluent_command)
 
         # Capture and process output
-        output = stdout.read().decode()
-        error = stderr.read().decode()
+        #output = stdout.read().decode()
+        #error = stderr.read().decode()
 
         ssh_client.close()
 
         
         
-        #c = Client("cadfemvmwindows", username="pavan", password="Cadfemindia@2023",encrypt=False)
-        #c.connect()
-        #c.cleanup()  # this is where the magic happens
-        #c.disconnect()
-        #try:
-        #    c.create_service()
-        #    stdout, stderr, rc = c.run_executable("cmd.exe",arguments="/c echo Hello World")
-        #finally:
-        #    c.remove_service()
-        #    c.disconnect()
+        c = Client("cadfemvmwindows", username="pavan", password="Cadfemindia@2023",encrypt=False)
+        c.connect()
+        c.cleanup()  # this is where the magic happens
+        c.disconnect()
+        try:
+            c.create_service()
+            stdout, stderr, rc = c.run_executable("cmd.exe",arguments="/c echo Hello World")
+        finally:
+            c.remove_service()
+            c.disconnect()
             
         return f"<pre>Output: {output}\nError: {error}</pre>"
     except Exception as e:
