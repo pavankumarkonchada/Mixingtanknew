c3eab3628a6f521188765eb4a6b1791106da5256
pavankumarkonchada <113668310+pavankumarkonchada@users.noreply.github.com>
2023-09-04 14:36:30 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask
 import paramiko
 #from pypsexec.client import Client
 import winrm
+import socket
+import Xlib.support.connect as xlib_connect
+import logging
+import select
 
+LOGGER = logging.getLogger(__name__)
 app = Flask(__name__)
 app.config['REQUEST_TIMEOUT'] = 300  # Set your desired timeout value in seconds
 
@@ -20,9 +25,66 @@ def launch_fluent():
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(vm_ip_address, username=username, password=password)
+
         
-        s = winrm.Session('13.68.168.34', auth=('pavan', 'Cadfemindia@2023'))
-        r = s.run_cmd('notepad.exe')
+        channels = {}
+        poller = select.poll()
+        def x11_handler(channel, (src_addr, src_port)):
+            x11_chanfd = channel.fileno()
+            local_x11_socket = xlib_connect.get_socket(*local_x11_display[:3])
+            local_x11_socket_fileno = local_x11_socket.fileno()
+            channels[x11_chanfd] = channel, local_x11_socket
+            channels[local_x11_socket_fileno] = local_x11_socket, channel
+            poller.register(x11_chanfd, select.POLLIN)
+            poller.register(local_x11_socket, select.POLLIN)
+            LOGGER.debug('x11 channel on: %s %s', src_addr, src_port)
+            transport._queue_incoming_channel(channel)
+        def flush_out(session):
+            while session.recv_ready():
+                sys.stdout.write(session.recv(4096))
+            while session.recv_stderr_ready():
+                sys.stderr.write(session.recv_stderr(4096))
+        # get local disply
+        local_x11_display = xlib_connect.get_display(os.environ['DISPLAY'])
+        # start x11 session
+        transport = ssh_client.get_transport()
+        session = transport.open_session()
+        session.request_x11(handler=x11_handler)
+        session.exec_command('xterm')
+        session_fileno = session.fileno()
+        poller.register(session_fileno, select.POLLIN)
+        # accept first remote x11 connection
+        transport.accept()
+        # event loop
+        while not session.exit_status_ready():
+            poll = poller.poll()
+            # accept subsequent x11 connections if any
+            if len(transport.server_accepts) > 0:
+                transport.accept()
+            if not poll: # this should not happen, as we don't have a timeout.
+                break
+            for fd, event in poll:
+                if fd == session_fileno:
+                    flush_out(session)
+                # data either on local/remote x11 socket
+                if fd in channels.keys():
+                    channel, counterpart = channels[fd]
+                    try:
+                        # forward data between local/remote x11 socket.
+                        data = channel.recv(4096)
+                        counterpart.sendall(data)
+                    except socket.error:
+                        channel.close()
+                        counterpart.close()
+                        del channels[fd]
+        print 'Exit status:', session.recv_exit_status()
+        flush_out(session)
+        session.close()
+
+
+
+        #s = winrm.Session('13.68.168.34', auth=('pavan', 'Cadfemindia@2023'))
+        #r = s.run_cmd('notepad.exe')
         
         # Construct the Fluent command with proper quoting
         fluent_command = r'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File C:\run.ps1'
